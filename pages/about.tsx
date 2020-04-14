0a8578b680e6781c5db414692a1b6ce3e86b4651
Tyler Storm <stormtv@mail.uc.edu>
2020-04-13 20:42:34 -0400

    Init Next.js

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function About() {
+  return (
+    <div>
+      <Layout>
+        <p>This is the about page</p>
+      </Layout>
+    </div>
+  );
+}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+export default function Index() {
+  return (
+    <div>
+      <Layout>
+        <p>Hello Next.js</p>
+      </Layout>
+    </div>
+  );
+}
