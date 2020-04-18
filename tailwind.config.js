ac3597a366b71303fea94f62cb8b15c7a220cd1c
Tyler Storm <stormtv@mail.uc.edu>
2020-04-17 22:37:34 -0400

    remove anything not needed

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,15 +16,6 @@ module.exports = {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
-      mono: [
-        '"Roboto Mono"',
-        'Menlo',
-        'Monaco',
-        'Consolas',
-        '"Liberation Mono"',
-        '"Courier New"',
-        'monospace',
-      ],
     },
     extend: {
       colors: {
@@ -35,28 +26,6 @@ module.exports = {
           100: '#304659',
           200: '#3B5266',
         },
-        green: {
-          50: '#F5FDFA',
-          100: '#CFF4E8',
-          200: '#38D39F',
-          300: '#35C494',
-        },
-        purple: {
-          100: '#D5DCF3',
-          200: '#617EDF',
-          300: '#5B76CF',
-        },
-        red: {
-          100: '#F3DAD9',
-          200: '#DD7373',
-          300: '#CD6B6B',
-        },
-      },
-      fontSize: {
-        xss: '0.625rem',
-      },
-      letterSpacing: {
-        superwide: '.2em',
       },
     },
   },
