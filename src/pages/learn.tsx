bdd4dd9b85deda64e4b2ace3832f09cc9ecfa073
Tyler Storm <stormtv@mail.uc.edu>
2020-04-20 12:51:37 -0400

    add other pages

--- src/pages/learn.tsx
+++ src/pages/learn.tsx
@@ -4,9 +4,9 @@ import Layout from '../components/layout'
 
 const Index: React.FC = () => {
   return (
-    <Layout title="Home">
+    <Layout title="Learn">
       <div className="flex items-center justify-center flex-grow text-3xl">
-        Home TODO
+        Learn TODO
       </div>
     </Layout>
   )
--- src/pages/terms.tsx
+++ src/pages/terms.tsx
@@ -4,9 +4,9 @@ import Layout from '../components/layout'
 
 const Index: React.FC = () => {
   return (
-    <Layout title="Home">
+    <Layout title="Terms">
       <div className="flex items-center justify-center flex-grow text-3xl">
-        Home TODO
+        Terms TODO
       </div>
     </Layout>
   )
