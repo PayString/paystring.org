626fa70ce8f0bde5c87ec1d64daf7e9002c638e7
Tyler Storm <stormtv@mail.uc.edu>
2020-05-20 18:22:59 -0400

    add mono fonts back in for blog markdown

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,15 @@ module.exports = {
         '"Segoe UI Symbol"',
         '"Noto Color Emoji"',
       ],
+      mono: [
+        '"Roboto Mono"',
+        'Menlo',
+        'Monaco',
+        'Consolas',
+        '"Liberation Mono"',
+        '"Courier New"',
+        'monospace',
+      ],
     },
     colors: {
       'orange-1': {
