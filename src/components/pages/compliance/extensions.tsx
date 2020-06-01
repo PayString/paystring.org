1d82da4c4a2679b386c4351189862042318c960c
Tyler Storm <stormtv@mail.uc.edu>
2020-06-01 15:00:20 -0400

    Refactor Compliance Extensions

--- src/components/pages/compliance/extensions.tsx
+++ src/components/pages/compliance/extensions.tsx
@@ -1,71 +1,65 @@
 import React from 'react'
 
+import IconCharity from '../../../../content/assets/compliance/extensions/charity.svg'
+import IconCommerce from '../../../../content/assets/compliance/extensions/commerce.svg'
+import IconGettingPaid from '../../../../content/assets/compliance/extensions/getting-paid.svg'
+import IconInvoicing from '../../../../content/assets/compliance/extensions/invoicing.svg'
+import IconP2P from '../../../../content/assets/compliance/extensions/p2p.svg'
+import IconUtilities from '../../../../content/assets/compliance/extensions/utilities.svg'
 import Wave from '../../common/wave'
-import IconCharity from '../../../../content/assets/compliance/extensions-charity.svg'
-import IconP2P from '../../../../content/assets/compliance/extensions-p2p.svg'
-import IconUtilities from '../../../../content/assets/compliance/extensions-utilities.svg'
-import IconGettingPaid from '../../../../content/assets/compliance/extensions-getting-paid.svg'
-import IconCommerce from '../../../../content/assets/compliance/extensions-commerce.svg'
-import IconInvoicing from '../../../../content/assets/compliance/extensions-invoicing.svg'
-
 
 const Extensions: React.FC = () => {
-
-	const sectionCopy = [
-		{
-			icon: <IconCharity />,
-			title: "Charity",
-			description: "Simpler to donate money to your favorite organizations."
-
-		},
-		{
-			icon: <IconP2P />,
-			title: "P2P Payments",
-			description: "Simpler to pay back your friends and family."
-
-		},
-		{
-			icon: <IconUtilities />,
-			title: "Utilities and Rent",
-			description: "Simpler to pay your bills."
-
-		},
-		{
-			icon: <IconGettingPaid />,
-			title: "Getting Paid",
-			description: "Simpler to earn a living."
-
-		},
-		{
-			icon: <IconCommerce />,
-			title: "Commerce",
-			description: "Simpler to get the things you need."
-
-		},
-		{
-			icon: <IconInvoicing />,
-			title: "Invoicing",
-			description: "Simpler for your business to get paid for what it makes."
-		},
-	];
+  const sectionCopy = [
+    {
+      icon: <IconCharity className="h-12.5 sm:h-14" />,
+      title: 'Charity',
+      description: 'Simpler to donate money to your favorite organizations.',
+    },
+    {
+      icon: <IconP2P className="h-12.5 sm:h-14" />,
+      title: 'P2P Payments',
+      description: 'Simpler to pay back your friends and family.',
+    },
+    {
+      icon: <IconUtilities className="h-12.5 sm:h-14" />,
+      title: 'Utilities and Rent',
+      description: 'Simpler to pay your bills.',
+    },
+    {
+      icon: <IconGettingPaid className="h-12.5 sm:h-14" />,
+      title: 'Getting Paid',
+      description: 'Simpler to earn a living.',
+    },
+    {
+      icon: <IconCommerce className="h-12.5 sm:h-14" />,
+      title: 'Commerce',
+      description: 'Simpler to get the things you need.',
+    },
+    {
+      icon: <IconInvoicing className="h-12.5 sm:h-14" />,
+      title: 'Invoicing',
+      description: 'Simpler for your business to get paid for what it makes.',
+    },
+  ]
 
   return (
-    <Wave
-      background="gray"
-      wave="white"
-      className="justify-center max-w-screen-xl ml-auto mr-auto"
-    >
-    	<center><h2 className="max-w-md -mt-16 mb-32 text-5xl font-bold">Other Extensions for PayID</h2></center>
-    	<div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-12 flex flex-wrap">
-    		{sectionCopy.map((copy) => {
-    			return (
-	    		<div className="p-8 mb-6 flex-grow">
-						<div className="mb-10">{copy.icon}</div>
-						<h3 className="text-3xl font-bold">{copy.title}</h3>
-						<p className="text-lg mt-4">{copy.description}</p>
-					</div> )
-				})}
-    	</div>
+    <Wave background="gray" wave="white">
+      <div className="flex justify-center w-full mb-12 sm:mb-52">
+        <h2 className="max-w-md text-3xl font-bold sm:text-5xl sm:text-center">
+          Other Extensions for PayID
+        </h2>
+      </div>
+      <div className="flex grid flex-wrap grid-cols-1 px-0 gap-15 sm:gap-30 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 sm-px-29">
+        {sectionCopy.map((copy) => {
+          return (
+            <div key={copy.title} className="flex-grow">
+              <div className="mb-5 sm:mb-10">{copy.icon}</div>
+              <h3 className="text-2xl font-bold sm:text-3xl">{copy.title}</h3>
+              <p className="mt-4 sm:text-xl">{copy.description}</p>
+            </div>
+          )
+        })}
+      </div>
     </Wave>
   )
 }
