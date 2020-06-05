e18a2e07c60aa79482787a9daa8be0a362c031b1
Jake Bonham <jake@docforce.io>
2020-06-05 09:15:00 -0700

    Added members grid

--- src/components/pages/company/members.tsx
+++ src/components/pages/company/members.tsx
@@ -1,15 +1,96 @@
 import React from 'react'
 
+import LogoBRD from '../../../../content/assets/company/members/logo-brd.svg'
+import LogoRipple from '../../../../content/assets/company/members/logo-ripple.svg'
 import Wave from '../../common/wave'
 
 const Members: React.FC = () => {
+  const iconClasses = 'h-52 mx-auto '
+  const sectionCopy = [
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Ripple',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'Crypto.com',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Blockchain.com',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'BRD',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Ripple',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'Crypto.com',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Blockchain.com',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'BRD',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Ripple',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'Crypto.com',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Blockchain.com',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'BRD',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Ripple',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'Crypto.com',
+    },
+    {
+      icon: <LogoRipple className={iconClasses} />,
+      title: 'Blockchain.com',
+    },
+    {
+      icon: <LogoBRD className={iconClasses} />,
+      title: 'BRD',
+    },
+  ]
+
   return (
-    <Wave
-      background="white"
-      wave="gray"
-      className="flex justify-center flex-grow text-3xl text-center"
-    >
-      Founding Members
+    <Wave background="white" wave="gray">
+      <div className="flex justify-center">
+        <h2 className="max-w-2xl mb-16 text-3xl font-bold sm:text-5xl sm:text-center sm:mb-28">
+          Founding Members
+        </h2>
+      </div>
+
+      <div className="grid grid-cols-2 gap-8 px-0 text-center lg:grid-cols-4 sm:gap-20">
+        {sectionCopy.map((copy) => {
+          return (
+            <div key={copy.title}>
+              {copy.icon}
+              <h2 className="text-2xl text-center">{copy.title}</h2>
+            </div>
+          )
+        })}
+      </div>
     </Wave>
   )
 }
