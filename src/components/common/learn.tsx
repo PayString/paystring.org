bf33b09ea3bcc3423314e55efa0be9251ec00086
Calvin <cjhunjhnuwala@gmail.com>
2020-06-09 15:56:00 -0700

    Home > Learn More Section (#23)
    * adding new margin var
    
    * built out the home> institutions section.
    
    * updated the common learn component to check for home prop, added svgs of other learn tiles
    
    * Fix Whitepaper.svg
    
    * Always show 8 items / Fix Travel Rule SVG
    
    * Clean up styles / remove unused import
    
    Co-authored-by: Tyler Storm <stormtv@mail.uc.edu>

--- src/components/common/learn.tsx
+++ src/components/common/learn.tsx
@@ -12,34 +12,50 @@ const Learn: React.FC<LearnProps> = (props) => {
     {
       bgStyle: {
         backgroundImage: 'url(/assets/learn/developers.svg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
       },
       title: 'Getting Started for Developers',
     },
     {
       bgStyle: {
         backgroundImage: 'url(/assets/learn/whitepaper.svg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
       },
       title: 'PayID Technical Whitepaper',
     },
     {
       bgStyle: {
+        backgroundImage: 'url(/assets/learn/api-reference.svg)',
+      },
+      title: 'API Reference',
+    },
+    {
+      bgStyle: {
+        backgroundImage: 'url(/assets/learn/standards.svg)',
+      },
+      title: 'PayID Standards',
+    },
+    {
+      bgStyle: {
         backgroundImage: 'url(/assets/learn/security.svg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
       },
       title: 'Security and Privacy',
     },
     {
       bgStyle: {
         backgroundImage: 'url(/assets/learn/travel-rule.svg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
       },
       title: 'Travel Rule and Compliance',
+    },
+    {
+      bgStyle: {
+        backgroundImage: 'url(/assets/learn/checkout.svg)',
+      },
+      title: 'Checkout',
+    },
+    {
+      bgStyle: {
+        backgroundImage: 'url(/assets/learn/xpring-sdk.svg)',
+      },
+      title: 'Xpring SDK for PayID',
     },
   ]
 
@@ -50,10 +66,14 @@ const Learn: React.FC<LearnProps> = (props) => {
       wave="orange"
       no-gutter
     >
-      <div className="flex sm:justify-center">
-        <h2 className="text-3xl font-bold mb-14 sm:text-5xl sm:text-center md:mb-25">
+      <div className="flex flex-col items-center sm:justify-center mb-14 md:mb-25">
+        <h2 className="text-3xl font-bold sm:text-5xl sm:text-center ">
           Learn More
         </h2>
+        <p className="max-w-xl mt-6 text-xl sm:mt-8 sm:text-center">
+          Discover everything you need to know about the free, open source PayID
+          protocol
+        </p>
       </div>
       <div className="flex justify-center">
         <div className="grid grid-cols-1 gap-8 sm:grid-cols-2 xl:grid-cols-4">
@@ -64,7 +84,7 @@ const Learn: React.FC<LearnProps> = (props) => {
                 target="_blank"
                 rel="noreferrer"
                 key={learnable.title}
-                className="transition duration-500 ease-in-out transform rounded-xl bg-blue-dark-900 focus:shadow-2xl hover:shadow-2xl focus:scale-105 hover:scale-105 focus:outline-none learnable"
+                className="transition duration-500 ease-in-out transform bg-no-repeat bg-cover rounded-xl bg-blue-dark-900 focus:shadow-2xl hover:shadow-2xl focus:scale-105 hover:scale-105 focus:outline-none learnable"
                 style={learnable.bgStyle}
               >
                 <h3 className="font-bold text-white learnable-title mx-13">
