79a51d16e438cae9b53678d0bc0548da3d38f5cb
Tyler Storm <4524823+Stormtv@users.noreply.github.com>
2020-06-09 20:57:10 -0400

    Upgrade Dependencies (#24)

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,8 @@
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable @typescript-eslint/naming-convention */
 // src/global.d.ts
 declare module '*.svg' {
   const value: React.StatelessComponent<React.SVGAttributes<SVGElement>>
   export default value
 }
-declare const __PATH_PREFIX__: string // eslint-disable-line no-underscore-dangle
+declare const __PATH_PREFIX__: string
