c23f2726605c5d9e8f3621da0363e895f78ba641
Tyler Storm <stormtv@mail.uc.edu>
2020-06-11 19:42:58 -0400

    auto run?

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,15 +1,16 @@
 const getDocsLocation = (): string => {
-  return 'https://dev.docs.payid.org'
-  // switch (process.env.GATSBY_RELEASE_ENV) {
-  //   case 'dev':
-  //     return 'https://dev.docs.payid.org'
-  //   case 'stage':
-  //     return 'https://stage.docs.payid.org'
-  //   case 'prod':
-  //     return 'https://docs.payid.org'
-  //   default:
-  //     return 'http://localhost:3000'
-  // }
+  console.log(process.env)
+  console.log(process.env.GATSBY_RELEASE_ENV)
+  switch (process.env.GATSBY_RELEASE_ENV) {
+    case 'dev':
+      return 'https://dev.docs.payid.org'
+    case 'stage':
+      return 'https://stage.docs.payid.org'
+    case 'prod':
+      return 'https://docs.payid.org'
+    default:
+      return 'http://localhost:3000'
+  }
 }
 
 export default getDocsLocation
