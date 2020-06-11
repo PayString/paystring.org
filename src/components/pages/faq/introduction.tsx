3d70ff80cee147f369cedbcff71e8e5f74272c0d
Tyler Storm <stormtv@mail.uc.edu>
2020-06-11 19:33:36 -0400

    Fix old icons
    TODO Compliance

--- src/components/pages/faq/introduction.tsx
+++ src/components/pages/faq/introduction.tsx
@@ -3,15 +3,8 @@ import React from 'react'
 import Wave from '../../common/wave'
 
 const Introduction: React.FC = () => {
-  const bgImgStyle = {
-    backgroundImage: 'url(/assets/faq/background.svg)',
-    backgroundRepeat: 'no-repeat',
-    backgroundSize: 'cover',
-    backgroundPosition: 'center top',
-  }
-
   return (
-    <Wave style={bgImgStyle} spacing="sm">
+    <Wave spacing="sm" waveClassNames="faq-bg">
       <div className="max-w-4xl pt-16 lg:pt-70">
         <h1 className="text-3xl font-bold sm:mb-10 mb-7 sm:text-h1 sm:leading-tighter">
           Frequently Asked Questions
