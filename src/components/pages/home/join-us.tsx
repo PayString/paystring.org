a9943f3dc58656cf84c17d1c2084f53a1546312b
Tyler Storm <stormtv@mail.uc.edu>
2020-06-24 15:47:36 -0400

    remove next week

--- src/components/pages/home/join-us.tsx
+++ src/components/pages/home/join-us.tsx
@@ -11,9 +11,8 @@ const JoinUs: React.FC = () => {
       className="flex items-center justify-center"
     >
       <div className="flex flex-col max-w-5xl text-2xl font-bold md:text-center md:text-4xl">
-        <div>
-          Join the first PayID Developer Conference next week on June 25th 2020
-        </div>
+        <div>Join the first PayID Developer Conference</div>
+        <div>June 25th 2020</div>
         <div className="mt-12 md:mt-16">
           <a
             href="https://ripple.com/lp/payid-dev-conference"
