fd005232e3dba0808b0b673f5c1ece2eeaa64bf3
Tyler Storm <stormtv@mail.uc.edu>
2020-06-25 19:02:58 -0400

    Change join-us

--- src/components/pages/home/join-us.tsx
+++ src/components/pages/home/join-us.tsx
@@ -11,15 +11,15 @@ const JoinUs: React.FC = () => {
       className="flex items-center justify-center"
     >
       <div className="flex flex-col max-w-5xl text-2xl font-bold md:text-center md:text-4xl">
-        <div>Join the first PayID Developer Conference</div>
-        <div>June 25th 2020</div>
+        <div>Participate in the PayID Hackathon</div>
+        <div>Over $24K in prizes</div>
         <div className="mt-12 md:mt-16">
           <a
-            href="https://ripple.com/lp/payid-dev-conference"
+            href="https://hackathon.payid.org/"
             target="_blank"
             rel="noreferrer"
           >
-            <Button label="Sign Up" />
+            <Button label="Register Now" />
           </a>
         </div>
       </div>
