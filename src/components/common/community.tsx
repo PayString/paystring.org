d2ac3446eeec291820db3490a6c82996604d0b7f
Jake Bonham <jake@docforce.io>
2020-08-26 10:26:57 -0700

    separate steps for email signup

--- src/components/common/community.tsx
+++ src/components/common/community.tsx
@@ -13,6 +13,7 @@ interface CommunityProps {
 }
 
 const Community: React.FC<CommunityProps> = (props) => {
+  const [step1, setStep1] = useState<boolean>(false)
   const [validForm, setValidForm] = useState<boolean>(false)
   const [role, setRole] = useState<string>('DEFAULT')
   const [firstName, setFirstName] = useState<string>()
@@ -58,6 +59,12 @@ const Community: React.FC<CommunityProps> = (props) => {
   const emailRegex = /^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
   const isValidEmail = (val: string | undefined): boolean => {
     return !!(val && emailRegex.test(val))
+  }
+
+  const showStep2 = () => {
+    if (isValidEmail(email)) {
+      setStep1(true)
+    }
   }
 
   useEffect(() => {
@@ -141,98 +148,140 @@ const Community: React.FC<CommunityProps> = (props) => {
             speaker events
           </div>
           <form onSubmit={handleSubmit} className="mt-16 md:mt-30">
-            <div className="relative px-6 mb-6 border-2 border-white rounded">
-              <select
-                defaultValue="DEFAULT"
-                id="role"
-                name="role"
-                onChange={(event) => setRole(event.target.value)}
-                className="w-full py-4 bg-transparent appearance-none focus:outline-none"
-              >
-                {roles.map((r) => (
-                  <option
-                    className="text-blue-dark-900"
-                    value={r.value}
-                    key={r.label}
-                    hidden={r.value === 'DEFAULT'}
-                  >
-                    {r.label}
-                  </option>
-                ))}
-              </select>
-              <SelectArrow className="absolute h-2 mt-2 text-white right-4 top-4" />
-              <label
-                htmlFor="role"
-                className={classNames(
-                  'absolute z-10 transition-all duration-300 ease-linear pointer-events-none text-white left-4 bg-orange-500 px-2',
-                  {
-                    '-top-3': role !== 'DEFAULT',
-                    'top-4': role === 'DEFAULT',
-                  },
-                )}
-              >
-                Role
-              </label>
+            {/* Step 2 */}
+            <div
+              id="step2"
+              className={classNames('transition-all duration-300 ease-linear', {
+                hidden: !step1,
+              })}
+            >
+              <div className="flex">
+                <InputField
+                  theme="orange"
+                  id="firstName"
+                  label="First Name"
+                  type="text"
+                  name="first-name"
+                  autoComplete="given-name"
+                  className="mr-3"
+                  required
+                  requiredText="First name is required"
+                  onChange={(e): void => setFirstName(e.currentTarget.value)}
+                />
+                <InputField
+                  theme="orange"
+                  id="lastName"
+                  label="Last Name"
+                  type="text"
+                  name="last-name"
+                  autoComplete="family-name"
+                  className="ml-3"
+                  onChange={(e): void => setLastName(e.currentTarget.value)}
+                />
+              </div>
+              <div className="relative px-6 mb-6 border-2 border-white rounded">
+                <select
+                  defaultValue="DEFAULT"
+                  id="role"
+                  name="role"
+                  onChange={(event) => setRole(event.target.value)}
+                  className="w-full py-4 bg-transparent appearance-none focus:outline-none"
+                >
+                  {roles.map((r) => (
+                    <option
+                      className="text-blue-dark-900"
+                      value={r.value}
+                      key={r.label}
+                      hidden={r.value === 'DEFAULT'}
+                    >
+                      {r.label}
+                    </option>
+                  ))}
+                </select>
+                <SelectArrow className="absolute h-2 mt-2 text-white right-4 top-4" />
+                <label
+                  htmlFor="role"
+                  className={classNames(
+                    'absolute z-10 transition-all duration-300 ease-linear pointer-events-none text-white left-4 bg-orange-500 px-2',
+                    {
+                      '-top-3': role !== 'DEFAULT',
+                      'top-4': role === 'DEFAULT',
+                    },
+                  )}
+                >
+                  Role
+                </label>
+              </div>
             </div>
-
-            <div className="flex">
+            {/* step 1 */}
+            <div
+              id="step1"
+              className={classNames({
+                hidden: step1,
+              })}
+            >
               <InputField
                 theme="orange"
-                id="firstName"
-                label="First Name"
-                type="text"
-                name="first-name"
-                autoComplete="given-name"
-                className="mr-3"
+                id="email"
+                label="Email"
+                type="email"
+                name="email"
+                autoComplete="email"
                 required
-                requiredText="First name is required"
-                onChange={(e): void => setFirstName(e.currentTarget.value)}
-              />
-              <InputField
-                theme="orange"
-                id="lastName"
-                label="Last Name"
-                type="text"
-                name="last-name"
-                autoComplete="family-name"
-                className="ml-3"
-                onChange={(e): void => setLastName(e.currentTarget.value)}
+                requiredText="Email address is required"
+                validate={isValidEmail}
+                invalidText="Invalid email address"
+                onChange={(e): void => {
+                  if (isValidEmail(e.currentTarget.value)) {
+                    setEmail(e.currentTarget.value)
+                  } else {
+                    setEmail(undefined)
+                  }
+                }}
               />
             </div>
 
-            <InputField
-              theme="orange"
-              id="email"
-              label="Email"
-              type="email"
-              name="email"
-              autoComplete="email"
-              required
-              requiredText="Email address is required"
-              validate={isValidEmail}
-              invalidText="Invalid email address"
-              onChange={(e): void => {
-                if (isValidEmail(e.currentTarget.value)) {
-                  setEmail(e.currentTarget.value)
-                } else {
-                  setEmail(undefined)
-                }
-              }}
-            />
+            <div
+              id="next"
+              className={classNames({
+                hidden: step1,
+              })}
+            >
+              <Button
+                label="Next"
+                variant="secondary"
+                className="mt-8 bg-white sm:hidden"
+                onClick={(): void => showStep2()}
+              />
+              <Button
+                variant="secondary"
+                label="Next"
+                size="lg"
+                className="hidden mx-auto mt-16 bg-white sm:block"
+                onClick={(): void => showStep2()}
+              />
+            </div>
 
-            <Button
-              disabled={!validForm}
-              label="Submit"
-              variant="secondary"
-              className="mt-8 bg-white sm:hidden"
-            />
-            <Button
-              disabled={!validForm}
-              variant="secondary"
-              label="Submit"
-              size="lg"
-              className="hidden mx-auto mt-16 bg-white sm:block"
-            />
+            <div
+              id="submit"
+              className={classNames({
+                hidden: !step1,
+              })}
+            >
+              <Button
+                disabled={!validForm}
+                label="Submit"
+                variant="secondary"
+                className="mt-8 bg-white sm:hidden"
+              />
+              <Button
+                disabled={!validForm}
+                variant="secondary"
+                label="Submit"
+                size="lg"
+                className="hidden mx-auto mt-16 bg-white sm:block"
+              />
+            </div>
           </form>
         </div>
       )}
