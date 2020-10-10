bea51b8264de37b108fb0f73042b260fe845b9d0
docforcedev <71043751+docforcedev@users.noreply.github.com>
2020-10-10 07:21:52 -0500

    Revert homepage video (#88)

--- src/components/pages/home/introduction.tsx
+++ src/components/pages/home/introduction.tsx
@@ -1,17 +1,19 @@
 import { withPrefix } from 'gatsby'
-import React, { useState } from 'react'
-import ReactPlayer from 'react-player'
+import React from 'react'
+// import ReactPlayer from 'react-player'
 
 import BetterUX from '../../../assets/home/introduction/better-ux.svg'
+import MobileIdentifier from '../../../assets/home/introduction/identifier-mobile.svg'
+import Identifier from '../../../assets/home/introduction/identifier.svg'
 import OpenSource from '../../../assets/home/introduction/open-source.svg'
-import Play from '../../../assets/home/introduction/play.svg'
+// import Play from '../../../assets/home/introduction/play.svg'
 import TechStack from '../../../assets/home/introduction/tech-stack.svg'
 import { getDocsLocation } from '../../../utils/config'
 import Button from '../../common/button'
 import Wave from '../../common/wave'
 
 const Introduction: React.FC = () => {
-  const [playing, setPlaying] = useState(false)
+  // const [playing, setPlaying] = useState(false)
   const bgImgStyle = {
     backgroundImage: `url(${withPrefix('/assets/home/background.svg')})`,
     backgroundRepeat: 'no-repeat',
@@ -70,7 +72,7 @@ const Introduction: React.FC = () => {
           </a>
           <Button to="contact" variant="secondary" label="Find Out More" />
         </div>
-        <div className="relative w-full h-full p-px mt-20 border-2 rounded-lg border-blue-dark-500 pb-16-9">
+        {/* <div className="relative w-full h-full p-px mt-20 border-2 rounded-lg border-blue-dark-500 pb-16-9">
           <ReactPlayer
             playing={playing}
             onReady={() => {
@@ -84,7 +86,9 @@ const Introduction: React.FC = () => {
             playIcon={<Play className="w-24" />}
             light={withPrefix('/assets/home/video-background.png')}
           />
-        </div>
+        </div> */}
+        <Identifier className="hidden mt-36 lg:block" />
+        <MobileIdentifier className="max-w-xs mt-24 lg:hidden" />
         <div className="grid grid-cols-1 gap-16 mt-24 lg:mt-36 lg:gap-28 lg:grid-cols-3">
           {sectionCopy.map((copy) => {
             return (
